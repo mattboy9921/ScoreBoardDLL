bff155c227c610b14c1cf4ff7a08cd949f3dbbb6
mattboy9921 <mattboy9921@aim.com>
2017-09-17 19:48:13 -0400

    Initial release

---
+++ src/main/java/DoubleLinkedList.java
@@ -0,0 +1,79 @@
+public class DoubleLinkedList {
+
+    private Node list;
+
+    public DoubleLinkedList() {
+        list = null;
+    }
+
+    public void addNode(Score s) {
+        Node newNode = new Node(s);
+        Node current = list;
+        if (list == null){
+            list = newNode;
+        }
+        else if (newNode.data.getScore() >= current.data.getScore()){
+            list = newNode;
+            newNode.next = current;
+            if (countNodes(list) > 10) {
+                removeLastNode();
+            }
+        }
+        else {
+            while (current.next != null && newNode.data.getScore() <= current.next.data.getScore()){
+                current = current.next;
+            }
+            if (current.next == null) {
+                if (countNodes(list) < 10) {
+                    current.next = newNode;
+                }
+            }
+            else {
+                newNode.next = current.next;
+                current.next = newNode;
+                if (countNodes(list) > 10) {
+                    removeLastNode();
+                }
+            }
+        }
+    }
+
+    public void removeLastNode() {
+        Node current = list;
+        Node previous = list;
+        while (current.next != null) {
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+    }
+
+    public void printLinkedList(){
+        Node current = list;
+        int count = 1;
+        while (current != null){
+            System.out.println(count + ": " + current.data.getScore());
+            current = current.next;
+            count++;
+        }
+    }
+
+    public int countNodes(Node list){
+        if (list.next == null) {
+            return 1;
+        }else{
+            return countNodes(list.next) + 1;
+        }
+    }
+
+    private class Node{
+
+        public Score data;
+        private Node next;
+
+        public Node(Score s){
+            data = s;
+            next = null;
+        }
+    }
+}
---
+++ src/main/java/ScoreBoardSSL.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class ScoreBoardDLL {
+
+    public static void main (String[] args) {
+
+        Random random = new Random();
+
+        // Create 20 scores and store them in an array
+        System.out.println("Here is a list of 20 scores:");
+        DoubleLinkedList scores = new DoubleLinkedList();
+        for (int count = 0; count < 20; count++) {
+            int score = random.nextInt(100) + 1;
+            System.out.println("Score " + (count + 1) + ": " + score);
+            scores.addNode(new Score(score));
+        }
+
+        System.out.println("\nThe top 10 scores are:");
+        scores.printLinkedList();
+    }
+}
